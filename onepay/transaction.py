67c2b3f4584bb42a96322f2c63435bf52fc74094
Rodrigo Ayala <rayalac@gmail.com>
2018-09-28 11:17:51 -0300

    Validate parameters when creating a transaction

--- onepay/transaction.py
+++ onepay/transaction.py
@@ -1,5 +1,13 @@
 # encoding: utf-8
 from __future__ import unicode_literals
+from enum import Enum
+from onepay.onepay import Onepay
+from onepay.cart import ShoppingCart
+
+class Channel(Enum):
+    WEB = "WEB"
+    MOBILE = "MOBILE"
+    APP = "APP"
 
 class Options(object):
     def __init__(self, api_key: str, shared_secret: str):
@@ -11,3 +19,16 @@ class Options(object):
 
         self.api_key = api_key
         self.shared_secret = shared_secret
+
+class Transaction(object):
+
+    @staticmethod
+    def create(shopping_cart: ShoppingCart, channel = None, external_unique_number = None, options = None):
+        if (channel != None and channel == Channel.APP and Onepay.get_app_scheme):
+            raise Exception("You need to set an app_scheme if you want to use the APP channel")
+
+        if (channel != None and channel == Channel.MOBILE and Onepay.get_callback_url):
+            raise Exception("You need to set valid callback if you want to use the MOBILE channel")
+
+        if not isinstance(shopping_cart, ShoppingCart) or (isinstance(shopping_cart, ShoppingCart) and not shopping_cart.get_items()):
+            raise Exception("Shopping cart is null or empty")
