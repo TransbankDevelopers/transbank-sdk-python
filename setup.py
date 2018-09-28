36e2d4719f70c21a44f7c0b9d5ba52d148e4a7c5
Rodrigo Ayala <rayalac@gmail.com>
2018-09-27 23:27:00 -0300

    Set Python 3.4 as the minimal running version

--- setup.py
+++ setup.py
@@ -111,7 +111,6 @@ setup(
         'License :: OSI Approved :: BSD License',
         'Programming Language :: Python',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
