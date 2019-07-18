486313bc50f85bc49c87afb95d3fbe89516a4d3e
Yrvin Escorihuela <yher136@gmail.com>
2019-07-17 22:33:54 -0400

    Add request in refund transaction to a refund

--- transbank/webpay/webpayplus/transaction.py
+++ transbank/webpay/webpayplus/transaction.py
@@ -2,6 +2,7 @@ import json
 
 from transbank.webpay.webpayplus.transaction_create_response import TransactionCreateResponse
 from transbank.webpay.webpayplus.transaction_commit_response import TransactionCommitResponse
+from transbank.webpay.webpayplus.transaction_refund_response import TransactionRefundResponse
 from transbank.webpay.webpayplus.webpayplus import *
 
 
@@ -87,3 +88,43 @@ class Transaction:
         json_data = response_json
         transaction_commit_response = TransactionCommitResponse(json_data)
         return transaction_commit_response
+
+    @classmethod
+    def refund(cls, token, amount, options=None):
+        commerce_code = WebpayPlus.commerce_code()
+        api_key = WebpayPlus.api_key()
+        base_url = WebpayPlus.integration_type_url()
+
+        if options is not None:
+            commerce_code = options.commerce_code
+            api_key = options.api_key
+            base_url = WebpayPlus.integration_type_url(options.integration_type)
+
+        headers = dict({
+            "Tbk-Api-Key-Id": commerce_code,
+            "Tbk-Api-Key-Secret": api_key,
+            "Content-Type": "application/json",
+        })
+
+        payload = dict({
+            "token": token,
+            "amount": amount
+        })
+
+        http_client = WebpayPlus.http_client
+        final_url = base_url + cls.COMMIT_TRANSACTION_ENDPOINT.format(token)
+        http_response = http_client.post(final_url, data=payload, headers=headers)
+        if (http_response.status_code < 200) or (http_response.status_code > 300) or http_response is None:
+            raise Exception('Could not obtain a response from the service', -1)
+        response_json = http_response.json()
+
+        if response_json.get('error_message') is not None:
+            raise Exception(response_json["error_message"])
+
+        json_data = response_json
+        transaction_refund_response = TransactionRefundResponse(json_data)
+        return transaction_refund_response
+
+
+
+
