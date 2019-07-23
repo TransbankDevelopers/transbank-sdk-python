733e51c2f264af3fb8314b6c7f7d7b67f673834f
Yrvin Escorihuela <yher136@gmail.com>
2019-07-23 15:10:07 -0400

    Write constructor and getters/setters

--- transbank/webpay/webpayplus_mall/detail.py
+++ transbank/webpay/webpayplus_mall/detail.py
@@ -10,3 +10,78 @@ class Detail:
         self._installments_amount = json_data.get('installments_amount', None)
         self._commerce_code = json_data.get('commerce_code', None)
         self._buy_order = json_data.get('buy_order', None)
+
+    @property
+    def amount(self):
+        return self._amount
+
+    @amount.setter
+    def amount(self, amount):
+        self._amount = amount
+
+    @property
+    def status(self):
+        return self._status
+
+    @status.setter
+    def status(self, status):
+        self._status = status
+
+    @property
+    def authorization_code(self):
+        return self._authorization_code
+
+    @authorization_code.setter
+    def authorization_code(self, authorization_code):
+        self._authorization_code = authorization_code
+
+    @property
+    def payment_type_code(self):
+        return self._payment_type_code
+
+    @payment_type_code.setter
+    def payment_type_code(self, payment_type_code):
+        self._payment_type_code = payment_type_code
+
+    @property
+    def response_code(self):
+        return self._response_code
+
+    @response_code.setter
+    def response_code(self, response_code):
+        self._response_code = response_code
+
+    @property
+    def installments_number(self):
+        return self._installments_number
+
+    @installments_number.setter
+    def installments_number(self, installments_number):
+        self._installments_number = installments_number
+
+    @property
+    def installments_amount(self):
+        return self._installments_amount
+
+    @installments_amount.setter
+    def installments_amount(self, installments_amount):
+        self._installments_amount = installments_amount
+
+    @property
+    def commerce_code(self):
+        return self._commerce_code
+
+    @commerce_code.setter
+    def commerce_code(self, commerce_code):
+        self._commerce_code = commerce_code
+
+    @property
+    def buy_order(self):
+        return self._buy_order
+
+    @buy_order.setter
+    def buy_order(self, buy_order):
+        self._buy_order = buy_order
+
+
+
