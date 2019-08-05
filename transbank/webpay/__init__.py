dd9dd640de24f7c61ac7f51c0ade18b5173261c9
Yrvin Escorihuela <yher136@gmail.com>
2019-08-05 15:17:50 -0400

    Fix import namespaces

--- transbank/webpay/__init__.py
+++ transbank/webpay/__init__.py
@@ -1,3 +1,4 @@
-from .webpayplus import webpayplus, transaction
+from transbank.webpay import webpayplus
+from transbank.webpay.webpayplus import transaction
 
 __all__ = ['webpayplus', 'transaction']
