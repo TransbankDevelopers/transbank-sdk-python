ba5c6cd12998e10a5cf8c726c6f872f746fbfbf7
Yrvin Escorihuela <yher136@gmail.com>
2019-08-05 16:05:52 -0400

    Refactor exception classes for better implementations

--- transbank/webpay/webpayplus/exceptions/transaction_create_exception.py
+++ transbank/webpay/webpayplus/exceptions/transaction_create_exception.py
@@ -2,24 +2,6 @@ from .transbank_exception import TransbankException
 
 
 class TransactionCreateException(TransbankException):
-    DEFAULT_MESSAGE = 'Transaction could not be created. Unidentified error.'
-
-    def __init__(self, error_code, original_exception, message=DEFAULT_MESSAGE):
-        self._error_code = error_code
-        self._message = message
-        self._original_exception = original_exception
-        msg = "{0} - {1} - {2}".format(self._error_code, self._message, self._message)
-        super(TransactionCreateException, self).__init__(msg)
-
-    @property
-    def error_code(self):
-        return self._error_code
-
-    @property
-    def message(self):
-        return self._message
-
-    @property
-    def previous(self):
-        return self._original_exception
+    def __init__(self, error_code, original_exception, message=None):
+        super(TransactionCreateException, self).__init__(error_code, original_exception, message)
 
--- transbank/webpay/webpayplus/exceptions/transbank_exception.py
+++ transbank/webpay/webpayplus/exceptions/transbank_exception.py
@@ -2,11 +2,10 @@
 class TransbankException(Exception):
     DEFAULT_MESSAGE = 'Transaction could not be created. Unidentified error.'
 
-    def __init__(self, error_code, original_exception, message=DEFAULT_MESSAGE):
-        self._error_code = error_code
-        self._message = message
-        self._original_exception = original_exception
-        msg = "{0} - {1} - {2}".format(self._error_code, self._message, self._message)
+    def __init__(self, error_code, original_exception, message):
+        if message is None:
+            message = TransbankException.DEFAULT_MESSAGE
+        msg = "{0} - {1} - {2}".format(error_code, message, original_exception)
         super(TransbankException, self).__init__(msg)
 
 
