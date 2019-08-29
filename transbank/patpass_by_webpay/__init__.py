d1f3454d5ca0b3b009b587edd70d3ec826f4fe92
Gonzalo Castillo Fajardo <goncafa@gmail.com>
2019-08-29 17:33:17 -0400

    Add  missing __init__.py for directory
    This file contains default commerce_code, api_key and integration_type
    for Patpass By Webpay

---
+++ transbank/patpass_by_webpay/__init__.py
@@ -0,0 +1,5 @@
+from transbank.common.integration_type import IntegrationType
+
+commerce_code = "597055555550"  # Default COMMERCE_CODE for Patpass By Webpay
+api_key = "579B532A7440BB0C9079DED94D31EA1615BACEB56610332264630D42D0A36B1C"  # Default API_KEY for Patpass By Webpay
+integration_type = IntegrationType.TEST # Default integration_type is TEST
