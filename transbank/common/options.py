fbaa5ae3f487f0a1d7e17ba25948f2909cbe49fa
Gonzalo Castillo Fajardo <goncafa@gmail.com>
2019-08-29 17:26:24 -0400

    Create Options class
    Webpay and Patpass Comercio have different headers name on the API for
    same thinks. We dont know if Transbank will fix it some day. By now we
    should manage this with an abstract Options class in order to each
    product could retrieve right header name when we make the http request.

---
+++ transbank/common/options.py
@@ -0,0 +1,55 @@
+from abc import ABC, abstractmethod
+
+from transbank.common.integration_type import IntegrationType
+
+
+class Options(ABC):
+    def __init__(self, commerce_code: str = None, api_key: str = None, integration_type: IntegrationType = None):
+        self.commerce_code = commerce_code
+        self.api_key = api_key
+        self.integration_type = integration_type
+
+    @abstractmethod
+    def get_header_commerce_code_name(self):
+        pass
+
+    @abstractmethod
+    def get_header_api_key_name(self):
+        pass
+
+    def set_commerce_code(self, commerce_code: str) -> None:
+        self._commerce_code = commerce_code
+
+    def get_commerce_code(self) -> str:
+        return self._commerce_code
+
+    def set_api_key(self, api_key: str) -> None:
+        self._api_key = api_key
+
+    def get_api_key(self) -> str:
+        return self._api_key
+
+    def set_integration_type(self, integration_type: IntegrationType) -> None:
+        self._integration_type = integration_type
+
+    def get_integration_type(self) -> IntegrationType:
+        return self._integration_type
+
+    @staticmethod
+    def is_empty(options: 'Options') -> bool:
+        return options is None or not options.commerce_code and not options.api_key and not options.integration_type
+
+    def __str__(self) -> str:
+        return "Options(commerce_code: {}, api_key: {}, integration_type: {})".format(self.commerce_code, self.api_key, self.integration_type)
+
+    commerce_code = property(get_commerce_code, set_commerce_code)
+    api_key = property(get_api_key, set_api_key)
+    integration_type = property(get_integration_type, set_integration_type)
+
+
+class WebpayOptions(Options):
+    def get_header_commerce_code_name(self):
+        return "Tbk-Api-Key-Id"
+
+    def get_header_api_key_name(self):
+        return "Tbk-Api-Key-Secret"
