c7bc562a57aa7cf2fb0907fe41ea402cb6d5a078
Eugenio Lopez <eugenionlopez@gmail.com>
2019-09-12 12:15:58 -0300

    WIP trx completa

--- transbank/transaccion_completa/schema.py
+++ transbank/transaccion_completa/schema.py
@@ -13,3 +13,26 @@ class CreateTransactionRequestSchema(Schema):
 class CreateTransactionResponseSchema(Schema):
     error_message = fields.Str()
     token = fields.Str()
+
+
+class CommitTransactionRequestSchema(Schema):
+    id_query_installments = fields.Str()
+    deferred_periods_index = fields.Str()
+    grace_period = fields.Str()
+
+
+class CommitTransactionResponseSchema(Schema):
+    vci = fields.Str()
+    amount = fields.Str()
+    status = fields.Str()
+    buy_order = fields.Str()
+    session_id = fields.Str()
+    card_detail = fields.Str()
+    accounting_date = fields.Str()
+    transaction_date = fields.Str()
+    authorization_code = fields.Str()
+    payment_type_code = fields.Str()
+    response_code = fields.Str()
+    installments_number = fields.Str()
+    installments_amount = fields.Str()
+    balance = fields.Str()
--- transbank/transaccion_completa/transaction.py
+++ transbank/transaccion_completa/transaction.py
@@ -9,3 +9,51 @@ from transbank.transaccion_completa.transaction_create_request import Transactio
 from transbank.transaccion_completa.schema import CreateTransactionRequestSchema, CreateTransactionResponseSchema
 from transbank.transaccion_completa.transaction_create_response import TransactionCreateResponse
 
+
+class Transaction(object):
+    @classmethod
+    def __base_url(clscls, integration_type: IntegrationType):
+        return "{}/rswebpaytransaction/api/webpay/v1.0/transactions".format(
+            webpay_host(integration_type))
+
+    @classmethod
+    def build_options(cls, options: Options = None) -> Options:
+        alt_options = WebpayOptions(transaccion_completa.commerce_code, transaccion_completa.api_key,
+                                    transaccion_completa.integration_type)
+        if options is not None:
+            alt_options.commerce_code = options.commerce_code or transaccion_completa.commerce_code
+            alt_options.api_key = options.api_key or transaccion_completa.api_key
+            alt_options.integration_type = options.integration_type or transaccion_completa.integration_type
+        return alt_options
+
+    @classmethod
+    def create(cls, buy_order: str, session_id: str, amount: float, card_number: str, cvv: str,
+               card_expiration_date: str,
+               options: Options = None) -> TransactionCreateResponse:
+        options = cls.build_options(options)
+        endpoint = cls.__base_url(options.integration_type)
+        request = TransactionCreateRequest(buy_order, session_id, amount, card_number, cvv, card_expiration_date)
+        response = requests.post(endpoint, data=CreateTransactionRequestSchema().dumps(request).data,
+                                 headers=HeadersBuilder.build(options))
+        response_json = response.text
+        response_dict = CreateTransactionResponseSchema().loads(response_json).data
+        if response.status_code in range(200, 299):
+            return TransactionCreateResponse(token=response_dict["token"])
+        raise TransactionCreateError(message=response_dict["error_message"])
+
+    @classmethod
+    def commit(cls, token: str, id_query_installments:str, deferred_period_index: int, grace_period: int,
+               options: Options = None):
+        options = cls.build_options(options)
+        endpoint = cls.__base_url(options.integration_type)
+        request = TransactionCommitRequest(buy_order, session_id, amount, card_number, cvv, card_expiration_date)
+        response = requests.post(endpoint, data=CreateTransactionRequestSchema().dumps(request).data,
+                                 headers=HeadersBuilder.build(options))
+        response_json = response.text
+        response_dict = CreateTransactionResponseSchema().loads(response_json).data
+        if response.status_code in range(200, 299):
+            return TransactionCreateResponse(token=response_dict["token"])
+        raise TransactionCreateError(message=response_dict["error_message"])
+
+
+
