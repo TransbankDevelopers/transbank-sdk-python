1a44c5fa005a06acf7487c14c39e29ddd65ed5b3
Eugenio Lopez <eugenionlopez@gmail.com>
2019-09-30 17:04:18 -0300

    Add more default values to params that are not always included

--- transbank/transaccion_completa/response/__init__.py
+++ transbank/transaccion_completa/response/__init__.py
@@ -11,34 +11,14 @@ class TransactionCreateResponse(object):
 
 class TransactionCommitResponse(object):
     def __init__(self,
-                 # vci: str,
+
                  amount: float, status: str, buy_order: str, session_id: str,
                  card_detail: CardDetail, accounting_date: str, transaction_date: str, authorization_code: str,
                  payment_type_code: str, response_code: str, installments_number: float,
-                 # installments_amount: float,
-                 # balance: float
+                 vci: str = None,
+                 installments_amount: float = None,
+                 balance: float = None
                  ):
-        # self.vci = vci
-        self.amount = amount
-        self.status = status
-        self.buy_order = buy_order
-        self.session_id = session_id
-        self.card_detail = card_detail
-        self.accounting_date = accounting_date
-        self.transaction_date = transaction_date
-        self.authorization_code = authorization_code
-        self.payment_type_code = payment_type_code
-        self.response_code = response_code
-        self.installments_number = installments_number
-        # self.installments_amount = installments_amount
-        # self.balance = balance
-
-
-class TransactionStatusResponse(object):
-    def __init__(self, vci: str, amount: float, status: str, buy_order: str, session_id: str,
-                 card_detail: CardDetail, accounting_date: str, transaction_date: str, authorization_code: str,
-                 payment_type_code: str, response_code: str, installments_number: float, installments_amount: float,
-                 balance: float):
         self.vci = vci
         self.amount = amount
         self.status = status
@@ -52,6 +32,26 @@ class TransactionStatusResponse(object):
         self.response_code = response_code
         self.installments_number = installments_number
         self.installments_amount = installments_amount
+        self.balance = balance
+
+
+class TransactionStatusResponse(object):
+    def __init__(self, amount: float, status: str, buy_order: str, session_id: str,
+                 card_detail: CardDetail, accounting_date: str, transaction_date: str, authorization_code: str,
+                 payment_type_code: str, response_code: str, installments_number: float = None,
+                 balance: float = None, vci: str = None):
+        self.vci = vci
+        self.amount = amount
+        self.status = status
+        self.buy_order = buy_order
+        self.session_id = session_id
+        self.card_detail = card_detail
+        self.accounting_date = accounting_date
+        self.transaction_date = transaction_date
+        self.authorization_code = authorization_code
+        self.payment_type_code = payment_type_code
+        self.response_code = response_code
+        self.installments_number = installments_number
         self.balance = balance
 
     def __repr__(self):
@@ -68,12 +68,10 @@ class TransactionStatusResponse(object):
         payment_type_code: {},
         response_code: {},
         installments_number: {},
-        installments_amount: {},
         balance:{}
         """.format(self.vci, self.amount, self.status, self.buy_order, self.session_id, self.card_detail,
-                   self.card_detail,
                    self.accounting_date, self.transaction_date, self.authorization_code, self.payment_type_code,
-                   self.response_code, self.installments_number, self.installments_amount, self.balance)
+                   self.response_code, self.installments_number, self.balance)
 
 
 class TransactionRefundResponse(object):
@@ -111,40 +109,3 @@ class TransactionInstallmentsResponse(object):
         id_query_installments: {},
         deferred_periods: {}
         """.format(self.installments_amount, self.id_query_installments, self.deferred_periods)
-
-
-class TransactionStatusResponse(object):
-    def __init__(self,
-                 amount: float, status: str, buy_order: str, session_id: str,
-                 card_detail: CardDetail, accounting_date: str, transaction_date: str, authorization_code: str,
-                 payment_type_code: str, response_code: str, installments_number: float,
-                 ):
-        self.amount = amount
-        self.status = status
-        self.buy_order = buy_order
-        self.session_id = session_id
-        self.card_detail = card_detail
-        self.accounting_date = accounting_date
-        self.transaction_date = transaction_date
-        self.authorization_code = authorization_code
-        self.payment_type_code = payment_type_code
-        self.response_code = response_code
-        self.installments_number = installments_number
-
-    def __repr__(self):
-        return """
-        amount: {},
-        status: {},
-        buy_order: {},
-        session_id: {},
-        card_detail: {},
-        accounting_date: {},
-        transaction_date: {},
-        authorization_code: {},
-        payment_type_code: {},
-        response_code: {},
-        installments_number: {},
-        """.format(self.amount, self.status, self.buy_order, self.session_id, self.card_detail,
-                   self.card_detail,
-                   self.accounting_date, self.transaction_date, self.authorization_code, self.payment_type_code,
-                   self.response_code, self.installments_number)
