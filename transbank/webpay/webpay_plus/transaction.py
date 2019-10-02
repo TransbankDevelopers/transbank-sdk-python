1593a1f83226eed07b9f807165306fdafe0d47c3
Gonzalo Castillo Fajardo <goncafa@gmail.com>
2019-10-02 17:09:09 -0300

    Implement create method

--- transbank/webpay/webpay_plus/transaction.py
+++ transbank/webpay/webpay_plus/transaction.py
@@ -1,9 +1,13 @@
 import requests
+from transbank.error.transaction_create_error import TransactionCreateError
 
 from transbank.common.headers_builder import HeadersBuilder
 from transbank.common.integration_type import IntegrationType, webpay_host
 from transbank.common.options import Options, WebpayOptions
-from transbank.webpay.webpay_plus.schema import TransactionStatusResponseSchema
+from transbank.webpay.webpay_plus.request import TransactionCreateRequest
+from transbank.webpay.webpay_plus.response import TransactionCreateResponse
+from transbank.webpay.webpay_plus.schema import TransactionStatusResponseSchema, TransactionCreateRequestSchema, \
+    TransactionCreateResponseSchema
 from transbank.error.transaction_status_error import TransactionStatusError
 from transbank.webpay.webpay_plus import default_commerce_code, default_api_key, default_integration_type, \
     TransactionStatusResponse
@@ -11,7 +15,7 @@ from transbank.webpay.webpay_plus import default_commerce_code, default_api_key,
 
 class Transaction(object):
     @classmethod
-    def __base_url(cls, integration_type: IntegrationType):
+    def __base_url(cls, integration_type: IntegrationType) -> str:
         return "{}/rswebpaytransaction/api/webpay/v1.0/transactions".format(
             webpay_host(integration_type))
 
@@ -25,6 +29,22 @@ class Transaction(object):
             alt_options.integration_type = options.integration_type or default_integration_type
 
         return alt_options
+
+    @classmethod
+    def create(cls, buy_order: str, session_id: str, amount: float, return_url: str, options: Options = None):
+        options = cls.build_options(options)
+        endpoint = cls.__base_url(options.integration_type)
+        request = TransactionCreateRequest(buy_order, session_id, amount, return_url)
+
+        response = requests.post(endpoint, data=TransactionCreateRequestSchema().dumps(request).data,
+                                 headers=HeadersBuilder.build(options))
+        json_response = response.text
+        dict_response = TransactionCreateResponseSchema().loads(json_response).data
+
+        if response.status_code not in (200, 299):
+            raise TransactionCreateError(message=dict_response["error_message"], code=response.status_code)
+
+        return TransactionCreateResponse(**dict_response)
 
     @classmethod
     def status(cls, token: str, options: Options = None):
