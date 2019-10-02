f9105131f6a95bf6fdba7734969cece4e5c3aa9d
Gonzalo Castillo Fajardo <goncafa@gmail.com>
2019-10-02 17:07:57 -0300

    Create TransactionCreateResponse class

--- transbank/webpay/webpay_plus/response/__init__.py
+++ transbank/webpay/webpay_plus/response/__init__.py
@@ -29,3 +29,12 @@ class TransactionStatusResponse(object):
             .format(self.vci, self.amount, self.status, self.buy_order, self.session_id, self.card_detail,
                     self.accounting_date, self.transaction_date, self.authorization_code, self.payment_type_code,
                     self.response_code, self.installments_number, self.installments_amount, self.balance)
+
+
+class TransactionCreateResponse(object):
+    def __init__(self, token: str, url: str):
+        self.token = token
+        self.url = url
+
+    def __repr__(self):
+        return "TransactionCreateResponse(token: {}, url: {})".format(self.token, self.url)
