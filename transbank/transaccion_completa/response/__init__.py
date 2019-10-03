2fc7e46a1fd3165850839d003fd22277acec6d64
Eugenio Lopez <eugenionlopez@gmail.com>
2019-10-03 10:52:41 -0300

    Add missing repr

--- transbank/transaccion_completa/response/__init__.py
+++ transbank/transaccion_completa/response/__init__.py
@@ -34,6 +34,40 @@ class TransactionCommitResponse(object):
         self.installments_amount = installments_amount
         self.balance = balance
 
+    def __repr__(self):
+        return """
+            vci: {}
+            amount: {}
+            status: {}
+            buy_order: {}
+            session_id: {}
+            card_detail: {}
+            accounting_date: {}
+            transaction_date: {}
+            authorization_code: {}
+            payment_type_code: {}
+            response_code: {}
+            installments_number: {}
+            installments_amount: {}
+            balance: {}
+        """.format(
+            self.vci,
+            self.amount,
+            self.status,
+            self.buy_order,
+            self.session_id,
+            self.card_detail,
+            self.accounting_date,
+            self.transaction_date,
+            self.authorization_code,
+            self.payment_type_code,
+            self.response_code,
+            self.installments_number,
+            self.installments_amount,
+            self.balance,
+        )
+
+
 
 class TransactionStatusResponse(object):
     def __init__(self, amount: float, status: str, buy_order: str, session_id: str,
