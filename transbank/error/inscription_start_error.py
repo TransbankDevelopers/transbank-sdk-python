67534697e6ddfb1577b150dad20d4c74d5d8724b
iescaida <iescaida@iescaidas-MacBook-Pro.local>
2019-10-07 15:26:34 -0300

    Fix class name

--- transbank/error/inscription_start_error.py
+++ transbank/error/inscription_start_error.py
@@ -2,6 +2,6 @@ from transbank.error.transbank_error import TransbankError
 
 
 class InscriptionStartError(TransbankError):
-    def __init__(self, message="Inscription Transaction could not be performed. Please verify given parameters",
+    def __init__(self, message="Inscription start could not be performed. Please verify given parameters",
                  code=0):
         super().__init__(message, code)
