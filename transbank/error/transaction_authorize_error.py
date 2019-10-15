5dfa9f88209cd51c6a8fabef45f2a010a5daddd4
iescaida <iescaida@iescaidas-MacBook-Pro.local>
2019-10-15 18:43:40 -0300

    Fix text for authorize exception error message

--- transbank/error/transaction_authorize_error.py
+++ transbank/error/transaction_authorize_error.py
@@ -2,5 +2,5 @@ from transbank.error.transbank_error import TransbankError
 
 
 class TransactionAuthorizeError(TransbankError):
-    def __init__(self, message="Transaction status could not be performed. Please verify given parameters", code=0):
+    def __init__(self, message="Transaction authorize could not be performed. Please verify given parameters", code=0):
         super().__init__(message, code)
