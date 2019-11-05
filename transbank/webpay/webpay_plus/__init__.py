b8d7adc03b7d453364b83e99ac27eb304505e11b
Cesar Mayo <cesar.mayo18@gmail.com>
2019-11-05 10:03:52 -0300

    Add commerce codes from webpay plus mall

--- transbank/webpay/webpay_plus/__init__.py
+++ transbank/webpay/webpay_plus/__init__.py
@@ -2,6 +2,6 @@ from transbank.common.integration_type import IntegrationType
 
 webpay_plus_default_commerce_code = "597055555532"  # Default COMMERCE_CODE for Webpay Plus
 webpay_plus_mall_default_commerce_code = "597055555535"  # Default COMMERCE_CODE for Webpay Plus Mall
-
+child_commerce_codes  = ['597055555536', '597055555537']
 default_api_key = "579B532A7440BB0C9079DED94D31EA1615BACEB56610332264630D42D0A36B1C"  # Default API_KEY for Webpay
 default_integration_type = IntegrationType.TEST  # Default integration_type is TEST
