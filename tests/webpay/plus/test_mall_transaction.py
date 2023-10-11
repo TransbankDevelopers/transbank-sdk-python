8a59a3c789994862472c457e24d08611b79e4c48
Isabel Anabalón Fuentes <isabel.anabalonfuentes@gmail.com>
2023-10-11 16:20:11 -0300

    test: add setup on test mall transaction

--- tests/webpay/plus/test_mall_transaction.py
+++ tests/webpay/plus/test_mall_transaction.py
@@ -1,16 +1,28 @@
 import unittest
 import random
+from unittest.mock import Mock
 from transbank.webpay.webpay_plus.mall_transaction import *
+from transbank.webpay.webpay_plus.request import *
 from transbank.common.integration_commerce_codes import IntegrationCommerceCodes
 
 
 class TransactionMallTestCase(unittest.TestCase):
-    return_url_mock = "https://url_return.com"
-    token_mock = 'ed11ddebcb970cd879e2b0ab843bd3c918ca8152e2ae51c038ac314aabc87ca7'
-    buy_order_child_refund_mock = 'abcdef1574772288'
 
-    def get_random_str(self):
-        return str(random.randrange(1000000, 99999999))
+    def setUp(self) -> None:
+        self.mall_buy_order_mock = 'mall_buy_order_mock_123'
+        self.session_id_mock = 'session_id_mock_123456789'
+        self.return_url_mock = "https://url_return.com"
+        self.amount1_mock = 1000
+        self.child1_commerce_code = IntegrationCommerceCodes.WEBPAY_PLUS_MALL_CHILD1
+        self.child1_buy_order = 'child_buy_order1_mock_123'
+        self.amount2_mock = 2000
+        self.child2_commerce_code = IntegrationCommerceCodes.WEBPAY_PLUS_MALL_CHILD2
+        self.child2_buy_order = 'child_buy_order2_mock_123'
+        self.token_mock = '01abf2be20aad1da804aeae1ed3062fb8fba108ee0e07f4d37181f51c3f6714d'
+        self.mock_response = Mock()
+        self.transaction = MallTransaction()
+        self.invalid_amount = -1000
+        self.authorization_code_mock = '123456'
 
     def get_mall_transaction_details(self):
         commerce_code_child_1 = IntegrationCommerceCodes.WEBPAY_PLUS_MALL_CHILD1
