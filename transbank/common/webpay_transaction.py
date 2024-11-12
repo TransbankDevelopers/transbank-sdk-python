e6692fc4e91d2436802d10ed19ae70877c5d6fb4
Miguel Vargas <mvarlic@gmail.com>
2024-11-12 13:07:37 -0500

    feat: replace configuration methods with static 'build' methods

--- transbank/common/webpay_transaction.py
+++ transbank/common/webpay_transaction.py
@@ -2,13 +2,15 @@ from transbank.common.options import WebpayOptions
 from transbank.common.integration_type import IntegrationType
 
 class WebpayTransaction(object):
-    def __init__(self, options: WebpayOptions = None):
+    def __init__(self, options: WebpayOptions):
         self.options = options
 
-    def configure_for_integration(self, commerce_code, api_key):
-        self.options = WebpayOptions(commerce_code, api_key, IntegrationType.TEST)
-        return self
+    @classmethod
+    def build_for_integration(cls, commerce_code, api_key):
+        options = WebpayOptions(commerce_code, api_key, IntegrationType.TEST)
+        return cls(options)
 
-    def configure_for_production(self, commerce_code, api_key):
-        self.options = WebpayOptions(commerce_code, api_key, IntegrationType.LIVE)
-        return self
+    @classmethod
+    def build_for_production(cls, commerce_code, api_key):
+        options = WebpayOptions(commerce_code, api_key, IntegrationType.LIVE)
+        return cls(options)
