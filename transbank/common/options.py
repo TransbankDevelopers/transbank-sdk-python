a6953a7abdd29aa6f656b7a49850e722f4e76ddd
Miguel Vargas <mvarlic@gmail.com>
2024-11-12 10:39:50 -0500

    feat: remove setters and default values ​​in the constructor

--- transbank/common/options.py
+++ transbank/common/options.py
@@ -4,10 +4,10 @@ from transbank.common.integration_type import IntegrationType
 
 
 class Options(ABC):
-    def __init__(self, commerce_code: str = None, api_key: str = None, integration_type: IntegrationType = None):
-        self.commerce_code = commerce_code
-        self.api_key = api_key
-        self.integration_type = integration_type
+    def __init__(self, commerce_code: str, api_key: str, integration_type: IntegrationType):
+        self._commerce_code = commerce_code
+        self._api_key = api_key
+        self._integration_type = integration_type
 
     @abstractmethod
     def header_commerce_code_name(self):
@@ -21,25 +21,13 @@ class Options(ABC):
     def commerce_code(self) -> str:
         return self._commerce_code
 
-    @commerce_code.setter
-    def commerce_code(self, commerce_code: str) -> None:
-        self._commerce_code = commerce_code
-
     @property
     def api_key(self) -> str:
         return self._api_key
 
-    @api_key.setter
-    def api_key(self, api_key: str) -> None:
-        self._api_key = api_key
-
     @property
     def integration_type(self) -> IntegrationType:
         return self._integration_type
-
-    @integration_type.setter
-    def integration_type(self, integration_type: IntegrationType) -> None:
-        self._integration_type = integration_type
 
     @staticmethod
     def is_empty(options: 'Options') -> bool:
